df694eb8714c49a4b0579fa44b62eef23cb0e079
Kanou Noboru <kanou@slogan.jp>
2015-03-30 10:15:17 +0900

    fix typo

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
+import os
 import tornado.ioloop
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("index.html")
 
 application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+    (r"/", MainHandler)
+    ],debug=True,
+    template_path=os.path.join(os.getcwd(),  "templates"),
+    static_path=os.path.join(os.getcwd(),  "static"),
+)
 
 if __name__ == "__main__":
     application.listen(8888)
+    print("Server is up ...")
     tornado.ioloop.IOLoop.instance().start()
